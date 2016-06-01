9526335bcdaf483cf861da45149749ebbfc4e3c2
Patrick Li <patrickli727@gmail.com>
2016-06-01 16:25:50 -0700

    added flux npm and started index page.

---
+++ react_frontend/components/product_index/product_index.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var ProductIndex = React.createClass({
+  componentDidMount: function() {
+
+  },
+
+  render: function() {
+    return (
+      <div>
+        Category Title
+      </div>
+    );
+  }
+});
+
+module.exports = ProductIndex;
