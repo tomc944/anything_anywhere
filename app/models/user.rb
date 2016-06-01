d5182f0aa1a081a148050d60c91b618de46b562b
Thomas Claiborne <thomas.claiborne3@gmail.com>
2016-06-01 14:00:39 -0700

    Added item model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::based
   validates :email, :session_token, :password_digest, presence: true
   validates :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
 
   attr_reader :password
 
