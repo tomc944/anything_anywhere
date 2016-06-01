6ca426c570981d8d3f014bf872b00b519e422fec
Thomas Claiborne <thomas.claiborne3@gmail.com>
2016-06-01 16:27:34 -0700

    Add cart in session update and built json endpoint for item

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :new]
-  resources :users, defaults: { format: :json }, only: :show
-
-  resource :session, only: [:create, :new, :destroy]
-  resources :current_user, defaults: { format: :json }, only: :index
-
   namespace :api, defaults: { format: :json } do
     resources :items
+
+    resources :users, only: [:create, :new]
+    resources :users, defaults: { format: :json }, only: :show
+
+    resource :session, only: [:create, :new, :destroy, :update]
+    resources :current_user, defaults: { format: :json }, only: :index
   end
 
   root 'static_pages#root'
