9505e00b59313a623a88a27a1ca7beed233d3d7d
Thomas Claiborne <thomas.claiborne3@gmail.com>
2016-06-01 14:43:10 -0700

    Add controllers

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,33 @@
+class Api::ItemsController < ApplicationController
+
+  def index
+    render :index
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      Item.create!
+      render json: @item
+    else
+      redirect_to new_api_item_url
+      render json: @item.errors.full_messages, status: 422
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def show
+    @user = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :weight,
+                                 :category, :dimension)
+  end
+
+end
