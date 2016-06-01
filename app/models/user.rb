b22379a5f64fa9c748f6b5f7d2c16630edce4b10
Thomas Claiborne <thomas.claiborne3@gmail.com>
2016-06-01 13:50:49 -0700

    Added user model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,36 @@
 class User < ActiveRecord::based
-  validates :email,
+  validates :email, :session_token, :password_digest, presence: true
+  validates :email, :session_token, uniqueness: true
 
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
