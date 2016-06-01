3afdff1c11bc02471b4bd548aab565d3d9b92f12
Thomas Claiborne <thomas.claiborne3@gmail.com>
2016-06-01 14:14:12 -0700

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  before_action :require_no_user!, only: [:new, :create]
+
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user
+      login_user!(user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = ["Incorrect username and/or password"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout_user!
+    render json: { wahooo: 'wahoooo!' }
+  end
+end
