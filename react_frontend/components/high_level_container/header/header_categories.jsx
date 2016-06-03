1e2c2cbf48955d9d2f6c647758d1ce906ce1469d
Patrick Li <patrickli727@gmail.com>
2016-06-03 16:37:46 -0700

    finished landing page positioning.

--- react_frontend/components/high_level_container/header/header_categories.jsx
+++ react_frontend/components/high_level_container/header/header_categories.jsx
@@ -4,14 +4,65 @@ var React = require('react'),
 var HeaderCategories = React.createClass({
   render: function() {
     return (
-      <div className="header-categories container">
-        <div className="col-xs-2 category-titles">Category</div>
-        <div className="col-xs-2 category-titles">Category</div>
-        <div className="col-xs-2 category-titles">Category</div>
-        <div className="col-xs-2 category-titles">Category</div>
-        <div className="col-xs-2 category-titles">Category</div>
-        <LoginDropdown/>
+      <div id="header-categories-accordion" role="tablist" aria-multiselectable="true">
+        <div className="panel panel-default">
+          <div className="panel-heading" role="tab" id="heading">
+            <div className="row">
+              <h4 className="panel-title col-xs-2">
+                <a data-toggle="collapse" data-parent="#header-categories-accordion" aria-expanded="false" href="#collapseOne" aria-controls="collapseOne">
+                  Category 1
+                </a>
+              </h4>
+              <h4 className="panel-title col-xs-2">
+                <a data-toggle="collapse" data-parent="#header-categories-accordion" aria-expanded="false" href="#collapseTwo" aria-controls="collapseTwo">
+                  Category 2
+                </a>
+              </h4>
+              <h4 className="panel-title col-xs-2">
+                <a data-toggle="collapse" data-parent="#header-categories-accordion" aria-expanded="false" href="#collapseThree" aria-controls="collapseThree">
+                  Category 3
+                </a>
+              </h4>
+              <h4 className="panel-title col-xs-2">
+                <a data-toggle="collapse" data-parent="#header-categories-accordion" aria-expanded="false" href="#collapseFour" aria-controls="collapseFour">
+                  Category 4
+                </a>
+              </h4>
+              <h4 className="panel-title col-xs-2">
+                <a data-toggle="collapse" data-parent="#header-categories-accordion" aria-expanded="false" href="#collapseFive" aria-controls="collapseFive">
+                  Category 5
+                </a>
+              </h4>
+              <h4 className="panel-title col-xs-2">
+                <a data-toggle="collapse" data-parent="#header-categories-accordion" aria-expanded="false" href="#collapseSix" aria-controls="collapseSix">
+                  Category 6
+                </a>
+              </h4>
+            </div>
+          </div>
+          <div id="collapseOne" className="panel-collapse collapse" role="tabpanel" aria-labelledby="heading">
+            I am the category 1 content. slkfsdlk;fj s;fkjs df;lkjds;lfk jsfl ;sldkfj ds;lfj sd;lfj sdl;kfj sa;ldfkj sdl;fk salfjs a;lkf sdl;kfj ;lsdfj ;slkfj sdl;kfj sl;kfjsl; fkfjkls;kfjs ;lafjls;k fkkds
+          </div>
+          <div id="collapseTwo" className="panel-collapse collapse" role="tabpanel" aria-labelledby="heading">
+            I am the category 2 content. slkfsdlk;fj s;fkjs df;lkjds;lfk jsfl ;sldkfj ds;lfj sd;lfj sdl;kfj sa;ldfkj sdl;fk salfjs a;lkf sdl;kfj ;lsdfj ;slkfj sdl;kfj sl;kfjsl; fkfjkls;kfjs ;lafjls;k fkkds
+          </div>
+          <div id="collapseThree" className="panel-collapse collapse" role="tabpanel" aria-labelledby="heading">
+            I am the category 3 content. slkfsdlk;fj s;fkjs df;lkjds;lfk jsfl ;sldkfj ds;lfj sd;lfj sdl;kfj sa;ldfkj sdl;fk salfjs a;lkf sdl;kfj ;lsdfj ;slkfj sdl;kfj sl;kfjsl; fkfjkls;kfjs ;lafjls;k fkkds
+          </div>
+          <div id="collapseFour" className="panel-collapse collapse" role="tabpanel" aria-labelledby="heading">
+            I am the category 4 content. slkfsdlk;fj s;fkjs df;lkjds;lfk jsfl ;sldkfj ds;lfj sd;lfj sdl;kfj sa;ldfkj sdl;fk salfjs a;lkf sdl;kfj ;lsdfj ;slkfj sdl;kfj sl;kfjsl; fkfjkls;kfjs ;lafjls;k fkkds
+          </div>
+          <div id="collapseFive" className="panel-collapse collapse" role="tabpanel" aria-labelledby="heading">
+            I am the category 5 content. slkfsdlk;fj s;fkjs df;lkjds;lfk jsfl ;sldkfj ds;lfj sd;lfj sdl;kfj sa;ldfkj sdl;fk salfjs a;lkf sdl;kfj ;lsdfj ;slkfj sdl;kfj sl;kfjsl; fkfjkls;kfjs ;lafjls;k fkkds
+          </div>
+          <div id="collapseSix" className="panel-collapse collapse" role="tabpanel" aria-labelledby="heading">
+            I am the category 6 content. slkfsdlk;fj s;fkjs df;lkjds;lfk jsfl ;sldkfj ds;lfj sd;lfj sdl;kfj sa;ldfkj sdl;fk salfjs a;lkf sdl;kfj ;lsdfj ;slkfj sdl;kfj sl;kfjsl; fkfjkls;kfjs ;lafjls;k fkkds
+          </div>
+        </div>
       </div>
+
+
+
     );
   }
 
