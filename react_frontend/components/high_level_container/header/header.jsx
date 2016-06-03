b3d51c0388465fc5b9dab560f4d633a7d07bcd39
Patrick Li <patrickli727@gmail.com>
2016-06-03 13:54:33 -0700

    added placements.

--- react_frontend/components/high_level_container/header/header.jsx
+++ react_frontend/components/high_level_container/header/header.jsx
@@ -1,19 +1,24 @@
 var React = require('react'),
     HeaderSearchBar = require('./header_search_bar'),
     HeaderCategories = require('./header_categories'),
-    LoginDropdown = require('./login_dropdown.jsx');
+    LoginDropdown = require('./login_dropdown'),
+    HeaderCart = require('./header_cart');
 
 var Header = React.createClass({
 
   render: function() {
     return (
-      <div>
-        <HeaderSearchBar/>
-        <div id="test">Anything Anywhere</div>
-        <HeaderCategories/>
-        <div>
-          Cart Button
+      <div className="header">
+        <div className="header-top-bar container-fluid">
+          <HeaderSearchBar/>
+          <div className="header-title col-xs-6">
+            <h3>ANYTHING ANYWHERE</h3>
+          </div>
+          <HeaderCart/>
         </div>
+
+
+        <HeaderCategories/>
         <LoginDropdown/>
       </div>
     );
---
+++ react_frontend/components/high_level_container/header/header_cart.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var HeaderCart = React.createClass({
+
+  render: function() {
+    return (
+      <div className="header-cart col-xs-3">
+        Cart Button
+      </div>
+    );
+  }
+
+
+});
+
+module.exports = HeaderCart;
