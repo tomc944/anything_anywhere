0af668f05c6f1b157d4816738ac15eea19698209
Patrick Li <patrickli727@gmail.com>
2016-06-04 14:34:09 -0700

    fixed user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::based
+class User < ActiveRecord::Base
   validates :email, :session_token, :password_digest, presence: true
   validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
