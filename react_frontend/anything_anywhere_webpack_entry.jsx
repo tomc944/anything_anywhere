85b990d13f13268badb267e15b62b2551f3a89f5
Patrick Li <patrickli727@gmail.com>
2016-06-04 17:05:12 -0700

    added operate page.

--- react_frontend/anything_anywhere_webpack_entry.jsx
+++ react_frontend/anything_anywhere_webpack_entry.jsx
@@ -6,7 +6,7 @@ var React = require('react'),
     HighLevelContainer = require('./components/high_level_container/high_level_container'),
     LandingPage = require('./components/landing_page/landing_page'),
     ItemIndex = require('./components/item_index/item_index'),
-    Info = require('./components/info/main_info'),
+    About = require('./components/info/about'),
     Contact = require('./components/info/contact'),
     HowWeOperate = require('./components/info/how_we_operate');
 
@@ -14,7 +14,7 @@ var routes = (
   <Route path="/" component={HighLevelContainer}>
     <IndexRoute component={LandingPage}/>
     <Route path="/products" component={ItemIndex}/>
-    <Route path="/info" component={Info}/>
+    <Route path="/about" component={About}/>
     <Route path="/contact" component={Contact}/>
     <Route path="/operate" component={HowWeOperate}/>
   </Route>
