264c17601b6ec1bae514fe398090c76112ed1b44
Thomas Claiborne <thomas.claiborne3@gmail.com>
2016-06-06 12:32:25 -0700

    Partition css out of application.css and into individual css files for components"

---
+++ react_frontend/components/info/CoinInformation.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var CoinInformation = React.createClass({
+  render: function() {
+    <div className="coin-information-container">
+      <div className="coin-information">
+
+
+
+      </div>
+    </div>
+  }
+})
