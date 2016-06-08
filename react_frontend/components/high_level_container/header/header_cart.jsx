e5e8c9dc4393a1487fd2c94c4057b34bea1baf53
Patrick Li <patrickli727@gmail.com>
2016-06-07 18:49:44 -0700

    added footer work.

--- react_frontend/components/high_level_container/header/header_cart.jsx
+++ react_frontend/components/high_level_container/header/header_cart.jsx
@@ -1,11 +1,16 @@
 var React = require('react');
 
 var HeaderCart = React.createClass({
+  getInitialState: function() {
+    return ({ itemCount: 0 });
+  },
 
   render: function() {
     return (
-      <div className="header-cart col-xs-3">
-        Cart Button <span className="badge">#</span>
+      <div className="col-xs-3">
+        <div className="header-cart">
+          Cart ({this.state.itemCount})
+        </div>
       </div>
     );
   }
---
+++ react_frontend/components/info/blog.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Blog = React.createClass({
+  render: function() {
+    return (
+      <div> Blog</div>
+    );
+  }
+});
+
+module.exports = Blog;
